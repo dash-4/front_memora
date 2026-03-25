1cc71a5fec75df31c69e9678588e214fd432a2d4
dash-4 <nikonova.aaa04@gmail.com>
2026-03-25 16:48:48 +0300

    back login

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link,  } from 'react-router-dom';
 import { Brain, Mail, Lock, ArrowRight } from 'lucide-react';
 import { authService } from '@/services/auth';
 import Button from '@/components/ui/Button';
 import toast from 'react-hot-toast';
 
 export default function Login() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const [formData, setFormData] = useState({
     username: '',
     password: '',
@@ -31,10 +31,12 @@ export default function Login() {
     try {
       await authService.login(formData.username, formData.password);
       toast.success('Вход выполнен успешно!', { id: 'login-success' });
-      navigate('/dashboard');
+      window.location.href = '/dashboard';
     } catch (error) {
       
       const errorData = error.response?.data;
+
+      
       
       if (errorData && typeof errorData === 'object') {
         setErrors(errorData);
